cea4463b13d8d2d206feb7db97c9db8619631a7d
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-10-28 00:22:05 -0700

    Set the basic files, directories and routes of the project

--- st-front-end/src/app/app.module.ts
+++ st-front-end/src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeViewComponent } from './home/home-view/home-view.component';
+import { AthleteViewComponent } from './athlete/athlete-view/athlete-view.component';
+import { OrganizerViewComponent } from './organizer/organizer-view/organizer-view.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeViewComponent },
+  { path: 'athlete', component: AthleteViewComponent },
+  { path: 'organizer', component: OrganizerViewComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    AthleteViewComponent,
+    OrganizerViewComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ st-front-end/src/app/organizer/organizer-view/organizer-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer-view',
+  templateUrl: './organizer-view.component.html',
+  styleUrls: ['./organizer-view.component.css']
+})
+export class OrganizerViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
