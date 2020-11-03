9bb646dbf8de70845be6c7c4f816959e3c3e599c
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-11-03 15:39:13 -0600

    Added the base for the athlete navbar and search bar

--- st-front-end/src/app/athlete/athlete-view/profile-widget/profile-widget.component.ts
+++ st-front-end/src/app/athlete/athlete-view/profile-widget/profile-widget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Athlete} from '../../../models/athlete.model';
 
 @Component({
   selector: 'app-profile-widget',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-widget.component.css']
 })
 export class ProfileWidgetComponent implements OnInit {
+  user: Athlete = new Athlete(
+    'Alvaro',
+    'Vargas',
+    'Costa Rican',
+    117730762,
+    20,
+    '3/4/2000',
+    'Costa Rica',
+    'Heredia',
+    'Belen',
+    'avargasm',
+    'https://www.hola.com/imagenes/cocina/recetas/20200617170335/mac-and-cheese/0-837-58/mac-cheese-adobe-m.jpg',
+    '123abc');
 
   constructor() { }
 
