a23f443f66fcb12a990981d230d13a4735ff2533
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-11-03 15:24:06 -0600

    Added the athlete model

---
+++ st-front-end/src/app/models/athlete.model.ts
@@ -0,0 +1,16 @@
+export class Athlete {
+  constructor(
+    public name: string,
+    public lastName: string,
+    public nationality: string,
+    public id: number,
+    public age: number,
+    public birthday: string,
+    public country: string,
+    public state: string,
+    public city: string,
+    public username: string,
+    public photo: string,
+    public password: string
+  ) {}
+}
