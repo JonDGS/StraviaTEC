9c9a69f0f337efa1a0cfad6faf96142c4405da81
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-11-06 21:31:39 -0600

    Added a way to see the general information for the current user and update it

--- st-front-end/src/app/athlete/athlete-view/athlete-settings/athlete-settings.component.ts
+++ st-front-end/src/app/athlete/athlete-view/athlete-settings/athlete-settings.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Athlete} from '../../../models/athlete.model';
+import {AthleteService} from '../../athlete.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-athlete-settings',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./athlete-settings.component.css']
 })
 export class AthleteSettingsComponent implements OnInit {
+  @ViewChild('updateProfileForm') profileForm: NgForm;
+  athlete: Athlete;
+  aName: string;
+  aLastName: string;
+  aNationality: string;
+  aCountry: string;
+  aState: string;
+  aCity: string;
 
-  constructor() { }
+  constructor(private aService: AthleteService) { }
 
   ngOnInit(): void {
+    this.athlete = this.aService.currentUser;
+    this.aName = this.athlete.name;
+    this.aLastName = this.athlete.lastName;
+    this.aNationality = this.athlete.nationality;
+    this.aCountry = this.athlete.country;
+    this.aState = this.athlete.state;
+    this.aCity = this.athlete.city;
   }
 
+  onUpdateProfile(): void {
+    console.log(this.profileForm);
+  }
 }
