bddb7ec7224486bf39b08c578e3b623d794a8a55
jonDorito <36711268+jonDorito@users.noreply.github.com>
2020-11-06 22:11:43 -0600

    Created model for onlineUser
    Build a model for an online user to use it for data migration to the database

---
+++ StraviaTECRestFullAPI/Models/OnlineUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StraviaTECRestFullAPI.Models
+{
+    public class OnlineUser
+    {
+        public string id;
+        public string token;
+        public string type;
+        public DateTime entry;
+        public DateTime expiryDate;
+
+    }
+}
