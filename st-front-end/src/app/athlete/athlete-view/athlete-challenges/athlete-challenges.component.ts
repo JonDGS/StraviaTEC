68cc262713c225c3d72f923890b2f709b94e967f
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-11-07 23:15:17 -0600

    Added the athlete challenges component

---
+++ st-front-end/src/app/athlete/athlete-view/athlete-challenges/athlete-challenges.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-athlete-challenges',
+  templateUrl: './athlete-challenges.component.html',
+  styleUrls: ['./athlete-challenges.component.css']
+})
+export class AthleteChallengesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
