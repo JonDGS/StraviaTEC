2b44ba41cf2f968b4f667c410cc2d9dcd207e284
jonDorito <36711268+jonDorito@users.noreply.github.com>
2020-11-08 22:36:17 -0600

    Added token manager

---
+++ StraviaTECRestFullAPI/Models/TokenManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Feria_Virtual_REST.Models
+{
+    public static class TokenManager
+    {
+
+        /**
+         * Description: Generates a random ID for an order
+         * Parameters:
+         * - size: size of string
+         * Return: Generate ID
+         */
+        public static string generateToken(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+    }
+}
