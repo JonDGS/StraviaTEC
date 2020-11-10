5d7c921de4d44e525633181e5d29adcd46fc024a
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-11-10 17:16:09 -0600

    Added the search and search-item component

---
+++ st-front-end/src/app/athlete/athlete-view/search/search-item/search-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-item',
+  templateUrl: './search-item.component.html',
+  styleUrls: ['./search-item.component.css']
+})
+
+/**
+ * This component is used for displaying the individual search item
+ */
+export class SearchItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ st-front-end/src/app/athlete/athlete-view/search/search.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+
+/**
+ * This component is for showing the searched items of a user search
+ */
+export class SearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
