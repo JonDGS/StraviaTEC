db4063ff82e34513a583479b6eec62ad7e9f1f3b
jonDorito <36711268+jonDorito@users.noreply.github.com>
2020-11-09 20:20:08 -0600

    Added ability to use database stored procedures
    - Added ability to check if a users password is correct

---
+++ StraviaTECRestFullAPI/Utilities/Connector.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StraviaTECRestFullAPI.Utilities
+{
+    public static class Connector
+    {
+        private static NpgsqlConnection connection = new NpgsqlConnection("Host=jongs.mynetgear.com;Port=5432;Username=Stravia_REST-API;Password=&fPcp8l<0oxmZQq]J;Database=StraviaTEC_DB");
+
+        public static bool checkUserPass(string username, string password)
+        {
+            connection.Open();
+
+            using (NpgsqlCommand cmd = new NpgsqlCommand("\"CheckPassword\"", connection))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("_username", username);
+                cmd.Parameters.AddWithValue("_password", password);
+                bool result = (bool)cmd.ExecuteScalar();
+
+                return result;
+            }
+
+        }
+    }
+}
